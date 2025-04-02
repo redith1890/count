f811ea606132d568339108f979d3301419dc7be7
Redith <ruben1001re@gmail.com>
2025-04-02 21:21:55 +0200

    rm

--- build.sh
+++ build.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-# Definir variables
 TARGET="count"
 SRC_DIR="src"
 BUILD_DIR="build"
 
-# Encontrar archivos fuente y generar la lista de objetos
 SOURCES=$(find $SRC_DIR -name "*.c")
 OBJECTS=$(echo "$SOURCES" | sed "s|$SRC_DIR/\(.*\).c|$BUILD_DIR/\1.o|")
 
-# Definir el compilador y las banderas de compilación
 CC="gcc"
 CFLAGS="-Wall -Wextra -std=c99 -O2"
 LIBS="-lm"
